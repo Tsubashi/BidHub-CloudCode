930fbf09c549a85a009aa1c621c49cc8b18092a1
Chandler Scott <8th.realm@gmail.com>
2018-04-08 12:07:13 +0000

    Add Braintree capabilities

--- index.js
+++ index.js
@@ -4,18 +4,24 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var braintree = require("braintree");
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
+var payment_prefix = "/payment"
+var gateway = braintree.connect({
+    environment:  braintree.Environment.Sandbox,
+    merchantId:   '5zbdx785v9729wyt',
+    publicKey:    'gbfn45btrnw526fk',
+    privateKey:   '44012e7e363864a6ac98db9038f89c05'
+});
 
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'UCRPCAuction',
   masterKey: process.env.MASTER_KEY || 'bba0f20f-7d2a-48c5-bd91-a0061da55985', //Add your master key here. Keep it secret!
+//  restAPIKey: process.env.REST_API_KEY || 'aa7118fa-67e8-4497-b45d-aa935647b3af', 
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   push: {
     android: {
@@ -42,6 +48,26 @@ var api = new ParseServer({
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
+
+// BrainTree Endpoints
+app.get(payment_prefix + "/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+
+app.post("/checkout", function (req, res) {
+  var nonceFromTheClient = req.body.payment_method_nonce;
+  gateway.transaction.sale({
+    amount: "10.00",
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      submitForSettlement: true
+    }
+  }, function (err, result) {
+    // TODO: Handle Errors
+  });
+});
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
